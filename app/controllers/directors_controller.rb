0461bccc35ec8570e83517f0a81da93e0fd1c925
felipeferla <164975883+felipeferla@users.noreply.github.com>
2024-04-30 04:59:20 +0000

    v3

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,4 +3,12 @@ class DirectorsController < ApplicationController
   def index
     render ({:template => "director_templates/list"})
   end
+
+  def do_details
+    @id_tag = params.fetch("id")
+    render ({:template => "director_templates/details"})
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/directors/:id", { :controller => "directors", :action => "do_details"})
+
 end
